daa924af442d531b7b01022415bc91786454223b
Muhammad Ahmed <muhammednch@gmail.com>
2023-02-15 18:22:03 -0500

    css setup

--- TicTacToe/toe.js
+++ TicTacToe/toe.js
@@ -1,1 +1,3 @@
 console.log('Your JS is linked up. Be the person you needed when you were little.')
+
+/*----- constants -----*//*----- app's state (variables) -----*//*----- cached element references -----*//*----- event listeners -----*//*----- functions -----*/
