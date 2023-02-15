c286050669436f0cad0d61bda7581b1f4d031096
Muhammad Ahmed <muhammednch@gmail.com>
2023-02-15 18:26:14 -0500

    js cache render added

--- TicTacToe/toe.js
+++ TicTacToe/toe.js
@@ -4,6 +4,7 @@ console.log('Your JS is linked up. Be the person you needed when you were little
 /*----- app's state (variables) -----*/
 let board;
 /*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'));
 /*----- event listeners -----*/
 /*----- functions -----*/
 function init() {
