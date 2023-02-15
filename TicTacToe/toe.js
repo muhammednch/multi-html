4e0db06d4942589eed4ef5d9efc012bb46bc22b5
Muhammad Ahmed <muhammednch@gmail.com>
2023-02-15 18:31:25 -0500

    js take turns step 1

--- TicTacToe/toe.js
+++ TicTacToe/toe.js
@@ -3,6 +3,7 @@ console.log('Your JS is linked up. Be the person you needed when you were little
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
 let board;
+let turn = 'X';
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll('#board div'));
 /*----- event listeners -----*/
@@ -19,6 +20,7 @@ init();
 function render() {
     board.forEach(function(mark, index){
     console.log(mark, index);
+    squares[index].textContent = mark;
 });
 };
 
