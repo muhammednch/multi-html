1e1594f2d8c0c02ee14dca5673f04afa27775e5a
Muhammad Ahmed <muhammednch@gmail.com>
2023-02-15 18:25:09 -0500

    js function render setup

--- TicTacToe/toe.js
+++ TicTacToe/toe.js
@@ -6,3 +6,17 @@ let board;
 /*----- cached element references -----*/
 /*----- event listeners -----*/
 /*----- functions -----*/
+function init() {
+    board = [
+        '', '', '',
+        '', '', '',
+        '', '', ''
+    ];
+};
+init();
+
+function render() {
+    board.forEach(function(mark, index){
+    console.log(mark, index);
+});
+};
