963410e1fa3178295a8c310087255ab78ca1582d
Muhammad Ahmed <muhammednch@gmail.com>
2023-02-15 18:23:22 -0500

    js comments

--- TicTacToe/toe.js
+++ TicTacToe/toe.js
@@ -1,3 +1,8 @@
 console.log('Your JS is linked up. Be the person you needed when you were little.')
 
-/*----- constants -----*//*----- app's state (variables) -----*//*----- cached element references -----*//*----- event listeners -----*//*----- functions -----*/
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+let board;
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
