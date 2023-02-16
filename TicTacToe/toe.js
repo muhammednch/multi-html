d8d0dff13dcc8e71a005b1e210a3bd17d616c842
Muhammad Ahmed <muhammednch@gmail.com>
2023-02-15 19:10:41 -0500

    js getting the getWinner function

--- TicTacToe/toe.js
+++ TicTacToe/toe.js
@@ -1,11 +1,25 @@
 console.log('Your JS is linked up. Be the person you needed when you were little.')
 
 /*----- constants -----*/
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 /*----- app's state (variables) -----*/
 let board;
 let turn = 'X';
+let win;
+win = board[0] && board[0] === board[1] && board[0] === board[2] ? board[0] : null;
+
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll('#board div'));
+const messages = document.querySelector('h2');
 /*----- event listeners -----*/
 document.getElementById('board').addEventListener('click', handleTurn);
 /*----- functions -----*/
@@ -23,6 +37,7 @@ function render() {
     console.log(mark, index);
     squares[index].textContent = mark;
 });
+messages.textContent = `It's ${turn}'s turn!`;
 };
 
 function init() {
@@ -38,8 +53,16 @@ function handleTurn(event) {
         return square === event.target;
     });
     board[idx] = turn;
+    win = getWinner();
     turn = turn === 'X' ? 'O' : 'X';
     render();
     console.log(board); //checking for connection
-    
+};
+
+function getWinner() {
+    let winner = null;
+    winningCombos.forEach(function(combo, index) {
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] &&   board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+    });
+    return winner;
 };
